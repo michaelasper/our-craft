000a5ed33d2bce686d257eb950ee6552ef8f6f96
Michael Asper <asper@utexas.edu>
2019-04-15 11:45:14 -0500

    added menger starter code

---
+++ src/menger.cc
@@ -0,0 +1,42 @@
+#include "menger.h"
+
+namespace {
+	const int kMinLevel = 0;
+	const int kMaxLevel = 4;
+};
+
+Menger::Menger()
+{
+	// Add additional initialization if you like
+}
+
+Menger::~Menger()
+{
+}
+
+void
+Menger::set_nesting_level(int level)
+{
+	nesting_level_ = level;
+	dirty_ = true;
+}
+
+bool
+Menger::is_dirty() const
+{
+	return dirty_;
+}
+
+void
+Menger::set_clean()
+{
+	dirty_ = false;
+}
+
+// FIXME generate Menger sponge geometry
+void
+Menger::generate_geometry(std::vector<glm::vec4>& obj_vertices,
+                          std::vector<glm::uvec3>& obj_faces) const
+{
+}
+
