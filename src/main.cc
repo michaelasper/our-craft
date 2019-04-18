45355db229854c7a0ee6d5737ed76dc6d37298a0
Michael Asper <asper@utexas.edu>
2019-04-18 04:06:18 -0500

    noise textures

--- src/main.cc
+++ src/main.cc
@@ -76,22 +76,14 @@ out vec4 world_position;
 void main()
 {
 
-	vec3 w_p1 = vec3(vs_world_pos[0]);
-    vec3 w_p2 = vec3(vs_world_pos[1]);
-    vec3 w_p3 = vec3(vs_world_pos[2]);
-    world_norm = vec4(normalize(cross(w_p2 - w_p1, w_p3 - w_p1)), 0.0);
-
-	vec3 p1 = gl_in[0].gl_Position.xyz;
-	vec3 p2 = gl_in[1].gl_Position.xyz;
-	vec3 p3 = gl_in[2].gl_Position.xyz;
-
-	vec3 edge1 = p2 - p1;
-	vec3 edge2 = p3 - p1;
-
-	normal = vec4(normalize(-cross(edge1,edge2)), 0.0);
+	vec3 side2 = (pos[2] - pos[0]).xyz;
+    vec3 side1 = (pos[1] - pos[0]).xyz;
+    vec3 normal3 = normalize(cross(side1,side2));
+    vec4 faceNormal = vec4(normal3[0], normal3[1], normal3[2], 0.0);
 
 	for (int n = 0; n < gl_in.length(); n++) {
 		light_direction = vs_light_direction[n];
+        normal = faceNormal;
 		world_position = pos[n];
 		gl_Position = projection  * gl_in[n].gl_Position;
 		EmitVertex();
@@ -104,43 +96,73 @@ const char* fragment_shader =
     R"zzz(#version 330 core
 flat in vec4 normal;
 flat in vec4 world_norm;
+in vec4 world_position;
 in vec4 light_direction;
 uniform mat4 view;
 
 out vec4 fragment_color;
+
+float mod289(float x){return x - floor(x * (1.0 / 289.0)) * 289.0;}
+vec4 mod289(vec4 x){return x - floor(x * (1.0 / 289.0)) * 289.0;}
+vec4 perm(vec4 x){return mod289(((x * 34.0) + 1.0) * x);}
+
+float noise(vec3 p){
+    vec3 a = floor(p);
+    vec3 d = p - a;
+    d = d * d * (3.0 - 2.0 * d);
+
+    vec4 b = a.xxyy + vec4(0.0, 1.0, 0.0, 1.0);
+    vec4 k1 = perm(b.xyxy);
+    vec4 k2 = perm(k1.xyxy + b.zzww);
+
+    vec4 c = k2 + a.zzzz;
+    vec4 k3 = perm(c);
+    vec4 k4 = perm(c + 1.0);
+
+    vec4 o1 = fract(k3 * (1.0 / 41.0));
+    vec4 o2 = fract(k4 * (1.0 / 41.0));
+
+    vec4 o3 = o2 * d.z + o1 * (1.0 - d.z);
+    vec2 o4 = o3.yw * d.x + o3.xz * (1.0 - d.x);
+
+    return o4.y * d.y + o4.x * (1.0 - d.y);
+}
+
+float fbm(vec3 x) {
+	float v = 0.0;
+	float a = 0.5;
+	vec3 shift = vec3(100);
+	for (int i = 0; i < 8; ++i) {
+		v += a * noise(x);
+		x = x * 2.0 + shift;
+		a *= 0.5;
+	}
+	return v;
+}
+
 void main()
 {
-    vec4 color = vec4(1.0,1.0,1.0,1.0); 
-    float thresh = 0.98;
-    float pos_x = dot(normal, vec4(1.0,0.0,0.0,0.0));
-    float pos_y = dot(normal, vec4(0.0,1.0,0.0,0.0));
-    float pos_z = dot(normal, vec4(0.0,0.0,1.0,0.0));
-    float neg_x = dot(normal, vec4(-1.0,0.0,0.0,0.0));
-    float neg_y = dot(normal, vec4(0.0,-1.0,0.0,0.0));
-    float neg_z = dot(normal, vec4(0.0,0.0,-1.0,0.0));
+  
+    float col = fbm(world_position.xyz);
 
-    if(pos_x > thresh){
-        color = vec4(1.0,0.0,0.0,1.0);
+    vec4 baseCol;
+    if(world_position.y < 0){
+        baseCol = vec4(0.1,0.4,0.8,1.0);
     }
-    if(pos_y > thresh){
-        color = vec4(0.0,1.0,0.0,1.0);
+    else if(world_position.y < 5){
+        baseCol = vec4(0.3,0.8,0.15,1.0);
+    }else{
+        baseCol = vec4(0.7,0.7,0.7,1.0);
     }
-    if(pos_z > thresh){
-        color = vec4(0.0,0.0,1.0,1.0);
-    }
-    if(neg_x > thresh){
-        color = vec4(1.0,0.0,0.0,1.0);
-    }
-    if(neg_y > thresh){
-        color = vec4(0.0,1.0,0.0,1.0);
-    }
-    if(neg_z > thresh){
-        color = vec4(0.0,0.0,1.0,1.0);
-    }
+
+
+    fragment_color = col * baseCol; 
+    fragment_color += vec4(0.1,0.1,0.1, 1.0);
 
     float dot_nl = dot(normalize(light_direction), view * normalize(normal));
-    dot_nl = clamp(dot_nl, 0.15, 1.0);
-    fragment_color = clamp(dot_nl * color, 0.05, 1.0);
+    dot_nl = clamp(dot_nl, 0.5, 1.0);
+    fragment_color = clamp( fragment_color * dot_nl, 0.0, 1.0);
+    // fragment_color[3] = 1.0;
 }
 )zzz";
 
